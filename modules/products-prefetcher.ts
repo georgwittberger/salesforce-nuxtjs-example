7878824c88bbd8a30b11ffd17d6ccde54c346436
Georg Wittberger <georg.wittberger@gmail.com>
2019-11-19 12:58:53 +0100

    Add logging to products prefetcher

--- modules/products-prefetcher.ts
+++ modules/products-prefetcher.ts
@@ -1,5 +1,8 @@
+import consola from 'consola'
 import { loadProducts } from '../server/products/products-store'
 
 export default async function productsPrefetcher(): Promise<void> {
+  consola.info('Prefetching products...')
   await loadProducts()
+  consola.success('Products successfully prefetched')
 }
